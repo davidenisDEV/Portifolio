81a66e6f615ad8be217391de048a048167cda421
David Denis <54123599+davidenisDEV@users.noreply.github.com>
2024-11-20 21:32:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,79 @@
-const $html = document.querySelector("html");
-const $checkbox = document.querySelector("#theme-switch");
 
-$checkbox.addEventListener("change", function () {
-    $html.classList.toggle("dark-mode");
+var typed = new Typed('#wrd', {
+    strings: ['Desenvolvedor Python', 'Full-Stack Developer', 'Analista de Dados', 'Especialista em Automação'],
+    typeSpeed: 100,
+    loop: true
 });
+
+
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navLinks.classList.toggle('active');
+});
+
+
+function openPopup() {
+    document.getElementById('popup').style.display = 'flex';
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
+
+function copyUPI() {
+    var upiId = document.getElementById('upi-id').textContent;
+    navigator.clipboard.writeText(upiId).then(function() {
+        alert('UPI ID copied to clipboard');
+    }, function() {
+        alert('Failed to copy UPI ID');
+    });
+}
+
+ 
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+document.addEventListener('DOMContentLoaded', function () {
+    const footer = document.querySelector('.footer');
+    const footerOffsetTop = footer.offsetTop;
+    const footerHeight = footer.offsetHeight;
+
+    function checkFooterVisibility() {
+        const scrollPosition = window.scrollY + window.innerHeight;
+        if (scrollPosition > footerOffsetTop + footerHeight / 4) {
+            footer.classList.add('footer-animation');
+        }
+    }
+
+    window.addEventListener('scroll', checkFooterVisibility);
+    checkFooterVisibility();
+});
+
+// my skill 
+const slides = document.querySelector('.slides');
+const images = document.querySelectorAll('.slides img');
+
+let index = 0;
+const slideInterval = 2000;
+
+function updateSlidePosition() {
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+document.querySelector('.next').addEventListener('click', () => {
+    index = (index + 1) % images.length;
+    updateSlidePosition();
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+    index = (index - 1 + images.length) % images.length;
+    updateSlidePosition();
+});
+
+function autoSlide() {
+    index = (index + 1) % images.length;
+    updateSlidePosition();
+}
+
+setInterval(autoSlide, slideInterval);
